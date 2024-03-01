5da2955a8a4e5f97b077408f2a28421f81b3ef9d
ahmed <kredahmed2@GMAIL.COM>
2024-03-01 15:32:25 +0200

    starting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,22 @@ import {
   StarsCanvas,
 } from "../components";
 const Home = () => {
-  return <div>3d portfolio</div>;
+  return (
+    <>
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </>
+  );
 };
 
 export default Home;
