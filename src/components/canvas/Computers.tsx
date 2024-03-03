b9b164d23a0ffa8d4f6d05c8fb2dea594212328b
ahmed <kredahmed2@GMAIL.COM>
2024-03-03 23:41:32 +0200

    starting about

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -6,7 +6,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }: { isMobile: boolean }) => {
-  const computer = useGLTF("./react_logo/scene.glb");
+  const computer = useGLTF("./desktop_pc/scene.glb");
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
