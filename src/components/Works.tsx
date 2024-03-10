019ea51d0e1f68ff09b89a9b45cfed4cf7c4c737
Ahmed hussen <149872862+sfjgnjhdskgas@users.noreply.github.com>
2024-03-10 16:12:27 +0200

    Update Works.tsx

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -86,7 +86,7 @@ const Works = () => {
 
       <div className="w-full flex">
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
+          variants={fadeIn("left", "spring", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Following projects showcases my skills and experience through
